0e9ec8327065a6b543664517433fa36dd74114a2
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-06-03 14:11:11 +0200

    New Motor Driver

--- motor_app/motor.c
+++ motor_app/motor.c
@@ -6,11 +6,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /* ioctl cmd */
-#define MOTOR_STOP        0x1
-#define MOTOR_RESET        0x2
-#define MOTOR_MOVE        0x3
-#define MOTOR_GET_STATUS    0x4
-#define MOTOR_SPEED        0x5
+#define MOTOR_STOP		0x1
+#define MOTOR_RESET		0x2
+#define MOTOR_MOVE		0x3
+#define MOTOR_GET_STATUS	0x4
+#define MOTOR_SPEED		0x5
+#define MOTOR_GOBACK	0x6
+#define MOTOR_CRUISE	0x7
 /* directional_attr */
 #define MOTOR_DIRECTIONAL_UP    0x0
 #define MOTOR_DIRECTIONAL_DOWN    0x1
@@ -25,20 +27,30 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-typedef struct {
-    int motor_directional;
-    int motor_move_steps;
-    int motor_move_speed;
-} motor_move_st;
 
 typedef struct {
-    int y_max;
-    int y_min;
-    int x_max;
-    int x_min;
-    int x_steps;
-    int y_steps;
-} motor_status_st;
+	int x;
+	int y;
+} motors_steps;
+
+enum motor_status {
+	MOTOR_IS_STOP,
+	MOTOR_IS_RUNNING,
+};
+
+typedef struct  {
+	int x;
+	int y;
+	enum motor_status status;
+	int speed;
+} motor_message;
+
+typedef struct  {
+	unsigned int x_max_steps;
+	unsigned int y_max_steps;
+	unsigned int x_cur_step;
+	unsigned int y_cur_step;
+} motor_reset_data;
 
 
 void sendCommand(int cmd, void *buffer) {
@@ -53,15 +65,26 @@ void setSpeed(int speed) {
 }
 
 void setMovement(int direction, int steps) {
-
-
-    motor_move_st motor_move;
-    motor_move.motor_directional = direction;
-    motor_move.motor_move_speed = 1000;
-    motor_move.motor_move_steps = steps;
+    int x = 0;
+    int y = 0;
+    switch(direction){
+        case MOTOR_DIRECTIONAL_UP:
+            y += steps;
+        break;
+        case MOTOR_DIRECTIONAL_DOWN:
+            y -= steps;
+        break;
+        case MOTOR_DIRECTIONAL_LEFT:
+            x -= steps;
+        break;
+        case MOTOR_DIRECTIONAL_RIGHT:
+            x += steps;
+        break;
+    }
+    motors_steps motor_move;
+    motor_move.x = x;
+    motor_move.y = y;
     sendCommand(MOTOR_MOVE, &motor_move);
-
-
 }
 
 void setStop() {
@@ -69,113 +92,35 @@ void setStop() {
 }
 
 void reset() {
-    sendCommand(MOTOR_RESET, 0);
+    motor_reset_data motor_move;
+    motor_move.x_max_steps = 0;
+    motor_move.y_max_steps = 0;
+    motor_move.x_cur_step = 0;
+    motor_move.y_cur_step = 0;
+    sendCommand(MOTOR_RESET, &motor_move);
 }
 
 
 void getStatus() {
-    motor_status_st status;
+    motor_message status;
     sendCommand(MOTOR_GET_STATUS, &status);
-
-
-    printf("y_max: %d\n", status.y_max);
-    printf("y_min: %d\n", status.y_min);
-    printf("x_max: %d\n", status.x_max);
-    printf("x_min: %d\n", status.x_min);
-    printf("x_steps: %d\n", status.x_steps);
-    printf("y_steps: %d\n", status.y_steps);
+    printf("x: %d\n", status.x);
+    printf("y: %d\n", status.y);
+    printf("speed: %d\n", status.speed);
 
 
 }
 
-void calibrate(int axis) {
-
-    setSpeed(1200);
+void calibrate() {
     reset();
-    motor_status_st status;
-    sendCommand(MOTOR_GET_STATUS, &status);
-
-    int direction1;
-    int direction2;
-    int *minField;
-    int *maxField;
-    int *stepField;
-    int *stepField_x;
-    int *stepField_y;
-    if (axis == AXIS_VERTICAL) {
-        direction1 = MOTOR_DIRECTIONAL_UP;
-        direction2 = MOTOR_DIRECTIONAL_DOWN;
-        maxField = &status.y_max;
-        minField = &status.y_min;
-        stepField = &status.y_steps;
-    } else if (axis == AXIS_HORIZONAL) {
-        direction1 = MOTOR_DIRECTIONAL_RIGHT;
-        direction2 = MOTOR_DIRECTIONAL_LEFT;
-        maxField = &status.x_max;
-        minField = &status.x_min;
-        stepField = &status.x_steps;
-    } else if (axis == AXIS_FORCIBLY)
-    {
-        stepField_x = &status.x_steps;
-        stepField_y = &status.y_steps;
-        minField = &status.x_min;
-        setMovement(MOTOR_DIRECTIONAL_LEFT, 2500);
-        while (*stepField_x != -2500 && *minField != 1 ) {
-            sendCommand(MOTOR_GET_STATUS, &status);
-        }
-        minField = &status.y_min;
-        setMovement(MOTOR_DIRECTIONAL_DOWN, 800);
-        while (*stepField_y != -800 && *minField != 1 ) {
-            sendCommand(MOTOR_GET_STATUS, &status);
-        }
-        reset();
-        setMovement(MOTOR_DIRECTIONAL_RIGHT, 1250);
-        while (*stepField_x != 1250) {
-            sendCommand(MOTOR_GET_STATUS, &status);
-        }
-        setMovement(MOTOR_DIRECTIONAL_UP, 400);
-        while (*stepField_y != 400) {
-            sendCommand(MOTOR_GET_STATUS, &status);
-        }
-        return 0;
-    } else{
-        exit(0);
-    }
-
-    setMovement(direction1, 5000);
-    while (*maxField != 1) {
-        sendCommand(MOTOR_GET_STATUS, &status);
-    }
-    // Go Down
-    int maxSteps = *stepField;
-    setMovement(direction2, 5000);
-    while (*minField != 1) {
-        sendCommand(MOTOR_GET_STATUS, &status);
-    }
-    int minSteps = *stepField;
-
-    // Calculate center:
-    int center = ((maxSteps - minSteps) / 2) + minSteps;
-
-    //sleep(1);
-    setMovement(direction1, 5000);
-    while (*stepField < center) {
-        sendCommand(MOTOR_GET_STATUS, &status);
-    }
-    setStop();
-
-    //printf("startSteps: %d\n", startSteps);
-    //printf("minSteps: %d\n", minSteps);
-    //printf("maxSteps: %d\n", maxSteps);
-    //printf("center: %d\n", center);
-    //printf("currentsteps: %d\n", status.y_steps);
 }
 
 
 int main(int argc, char *argv[]) {
 
+
     char direction = 0;
-    int stepsize = 100;
+    int stepsize = 1;
 
     // Parse Arguments:
     int c;
@@ -207,20 +152,15 @@ int main(int argc, char *argv[]) {
         case 'r':
             setMovement(MOTOR_DIRECTIONAL_RIGHT, stepsize);
             break;
-        case 'v':
-            calibrate(AXIS_VERTICAL);
-            break;
-        case 'h':
-            calibrate(AXIS_HORIZONAL);
-            break;
-        case 'f':
-            calibrate(AXIS_FORCIBLY);
+        case 'c':
+            calibrate();
             break;
         case 's':
             setStop();
             break;
         default:
-            printf("Invalid Direction Argument %c\n", c);
+            printf("Invalid Direction Argument %c\n", direction);
+            getStatus();
             exit(EXIT_FAILURE);
     }
     getStatus();
