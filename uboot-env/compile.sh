7258e67058d658d4c2128c759c139134e6f396b4
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-06-24 14:52:38 +0200

    Uboot ENV

---
+++ uboot-env/compile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+TOOLCHAIN=$(pwd)/../toolchain/bin
+CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
+export CC=${CROSS_COMPILE}gcc
+export LD=${CROSS_COMPILE}ld
+export CFLAGS="-muclibc -O2"
+export CPPFLAGS="-muclibc -O2"
+export LDFLAGS="-muclibc -O2"
+make
