939d5950b079aefbe7cedc50a0eae6c37f04bf89
puddly <32534428+puddly@users.noreply.github.com>
2018-07-23 00:37:54 -0400

    Added FreeType2 compilation script into compile-libraries.sh, for now

--- compile-libraries.sh
+++ compile-libraries.sh
@@ -1,6 +1,19 @@
 ./lame/compile.sh
 ./live/compile.sh
-./freetype2/compile.sh
 cd opus
 ./compile.sh
+cd ..
+
+cd freetype2
+TOOLCHAIN=$(pwd)/../toolchain/bin
+CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
+make clean
+export CC=${CROSS_COMPILE}gcc
+export CFLAGS="-muclibc -O2"
+export CPPFLAGS="-muclibc -O2"
+export LDFLAGS="-muclibc -O2"
+
+./autogen.sh
+./configure --host=mips-linux-gnu --without-harfbuzz --without-png --without-zlib --prefix=$(pwd)/../_install
+make install
 cd ..
