dcb12d7a0470f18392dc6b0274ab544c93dfdf78
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-08-08 20:54:30 +0200

    New Busybox

--- compile_busybox.sh
+++ compile_busybox.sh
@@ -3,9 +3,6 @@ TOOLCHAIN=$(pwd)/toolchain/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CC=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
-export CFLAGS="-muclibc -O2"
-export CPPFLAGS="-muclibc -O2"
-export LDFLAGS="-muclibc -O2"
 
 cd busybox/
 make clean
@@ -17,6 +14,8 @@ sed -e 's/CONFIG_FSYNC=y/CONFIG_FSYNC=n/' -i .config
 sed -e 's/CONFIG_SYNC=y/CONFIG_SYNC=n/' -i .config
 
 # Enable some features (note the /c which changes the entire line)
+sed -e '/CONFIG_FLASH_LOCK/c\CONFIG_FLASH_LOCK=y' -i .config
+sed -e '/CONFIG_FLASH_UNLOCK/c\CONFIG_FLASH_UNLOCK=y' -i .config
 sed -e '/CONFIG_FLASH_ERASEALL/c\CONFIG_FLASH_ERASEALL=y' -i .config
 
 make CROSS_COMPILE=$CROSS_COMPILE
