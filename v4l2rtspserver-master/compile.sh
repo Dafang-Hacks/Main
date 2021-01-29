791782fa10104b3d053c63941b02c2599a106543
Chris Knight <cdknight@hotcat.org>
2021-01-28 20:09:00 -0800

    disable motion-triggered system calls, replace with mqtt messages to localhost

--- v4l2rtspserver-master/compile.sh
+++ v4l2rtspserver-master/compile.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-TOOLCHAIN=$(pwd)/../toolchain/bin
+# uncomment to disable the detectionOn/Off/Tracking script execution, instead rely on MQTT
+NO_MOTION_SYSTEM_CALLS="-UMOTION_SYSTEM_CALLS"
+
+ROOTPATH=$(git rev-parse --show-toplevel)
+echo "setting $ROOTPATH"
+export TOOLCHAIN=${ROOTPATH}/toolchain/bin
+CROSS_PREFIX=mips-linux-gnu-
+export CC=${TOOLCHAIN}/${CROSS_PREFIX}gcc
+export LD=${TOOLCHAIN}/${CROSS_PREFIX}ld
+export CCLD=${TOOLCHAIN}/${CROSS_PREFIX}ld
+export CXX=${TOOLCHAIN}/${CROSS_PREFIX}g++
+export CPP=${TOOLCHAIN}/${CROSS_PREFIX}cpp
+export CXXCPP=${TOOLCHAIN}/${CROSS_PREFIX}cpp
+export AR=${TOOLCHAIN}/${CROSS_PREFIX}ar
+export INSTALL=${ROOTPATH}/_install
 export CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
-export CC=${CROSS_COMPILE}gcc
-export LD=${CROSS_COMPILE}g++
-export PKG_CONFIG_PATH="$../_install/lib/pkgconfig"
-export LIBRARY_PATH=../_install/lib
-export CFLAGS="-muclibc -O3 -lrt -I../v4l2rtspserver-tools -I../_install/include/freetype2 -I../../_install/include/"
-export CPPFLAGS="-muclibc -O3 -lrt -I../v4l2rtspserver-tools -I../_install/include/freetype2 -I../../_install/include/ -std=c++11"
-export LDFLAGS="-muclibc -O3 -lrt -lstdc++ -lpthread -ldl"
+export PKG_CONFIG_PATH=$ROOTPATH/_install/lib/pkgconfig
+export LIBRARY_PATH=$ROOTPATH/_install/lib
+export CFLAGS="-muclibc -O3 -lrt -I../v4l2rtspserver-tools -I../_install/include/freetype2 -I../../_install/include/ ${NO_MOTION_SYSTEM_CALLS}"
+export CPPFLAGS="-muclibc -O3 -lrt -I../v4l2rtspserver-tools -I../_install/include/freetype2 -I../../_install/include/ -std=c++11 ${NO_MOTION_SYSTEM_CALLS}"
+export LDFLAGS="-muclibc -O3 -L${LIBRARY_PATH} -lrt -lstdc++ -lpthread -ldl -lmosquitto -lssl -ltls -lcrypto"
 rm CMakeCache.txt
 rm -r CMakeFiles
 cmake -DCMAKE_TOOLCHAIN_FILE="./dafang.toolchain"  -DCMAKE_INSTALL_PREFIX=./_install --debug-output && make VERBOSE=1 -j4 install
