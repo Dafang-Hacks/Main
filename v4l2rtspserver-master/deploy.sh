d3d90c4ea84585d36d79233fd46fe56b31a16608
Chris Knight <cdknight@hotcat.org>
2021-01-30 15:21:39 -0800

    update to deploy.sh

--- v4l2rtspserver-master/deploy.sh
+++ v4l2rtspserver-master/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-HOST=10.0.0.17
+echo "NOTE: Be sure to turn off the RTSP server."
+
 if [ "$#" -ge 1 ]; then
     HOST=$1
 else
@@ -10,13 +11,11 @@ fi
 echo -n 'Password for your root user? (not echoed) '
 read -s PASS
 
-if [ $? == 0 ]; then
-    echo Copying to ${HOST} v4l2rtspserver-master
-    ftp-upload --passive -h ${HOST} -u root --password ${PASS} -d /system/sdcard/bin/ _install/bin/*
- 
-    for lib in libBasicUsageEnvironment.so libUsageEnvironment.so libgroupsock.so libliveMedia.so
-    do
-        echo Copying to ${HOST} ${lib}
-        ftp-upload --passive -h ${HOST} -u root --password ${PASS} -d /system/sdcard/lib/ ../_install/lib/${lib}
-    done
-fi
+echo Copying to ${HOST} v4l2rtspserver-master
+ftp-upload --passive -h ${HOST} -u root --password ${PASS} -d /system/sdcard/bin/ _install/bin/*
+
+for lib in libBasicUsageEnvironment.so libUsageEnvironment.so libgroupsock.so libliveMedia.so
+do
+    echo Copying to ${HOST} ${lib}
+    ftp-upload --passive -h ${HOST} -u root --password ${PASS} -d /system/sdcard/lib/ ../_install/lib/${lib}
+done
